5f96a6bfadd1e42100bd8dcd2a6689ca3c4f437b
yutomasamura <yuto.masumura@favy.co.jp>
2019-10-28 10:09:00 +0900

    get_bitcoin_data_by_ticker_api

--- main.go
+++ main.go
@@ -5,10 +5,16 @@ import (
 	"cryptocurrency_trading/config"
 	"cryptocurrency_trading/utils"
 	"fmt"
+	"time"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
 	fmt.Println(apiClient.GetBalance())
+	ticker, _ := apiClient.GetTicker("BTC_USD")
+	fmt.Println(ticker)
+	fmt.Println(ticker.GetMidPrice())
+	fmt.Println(ticker.DateTime())
+	fmt.Println(ticker.TruncateDateTime(time.Hour))
 }
