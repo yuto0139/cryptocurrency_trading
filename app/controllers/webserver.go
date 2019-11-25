64ecc6335204d792a93b64def8b6f3a64554dee3
yutomasamura <yuto.masumura@favy.co.jp>
2019-11-25 10:22:37 +0900

    create_data_frame_and_display_tick_chart

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"net/http"
 
+	"cryptocurrency_trading/app/models"
 	"cryptocurrency_trading/config"
 )
 
@@ -12,7 +13,12 @@ var templates = template.Must(template.ParseFiles("app/views/google.html"))
 
 // viewChartHandler ...
 func viewChartHandler(w http.ResponseWriter, r *http.Request) {
-	err := templates.ExecuteTemplate(w, "google.html", nil)
+	limit := 100
+	duration := "1m"
+	durationTime := config.Config.Durations[duration]
+	df, _ := models.GetAllCandle(config.Config.ProductCode, durationTime, limit)
+
+	err := templates.ExecuteTemplate(w, "google.html", df.Candles)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
---
+++ app/models/dfcandle.go
@@ -0,0 +1,64 @@
+package models
+
+import "time"
+
+// DataFrameCandle ...
+type DataFrameCandle struct {
+	ProductCode string        `json:"product_code"`
+	Duration    time.Duration `json:"duration"`
+	Candles     []Candle      `json:"candles"`
+}
+
+// Times ...
+func (df *DataFrameCandle) Times() []time.Time {
+	s := make([]time.Time, len(df.Candles))
+	for i, candle := range df.Candles {
+		s[i] = candle.Time
+	}
+	return s
+}
+
+// Opens ...
+func (df *DataFrameCandle) Opens() []float64 {
+	s := make([]float64, len(df.Candles))
+	for i, candle := range df.Candles {
+		s[i] = candle.Open
+	}
+	return s
+}
+
+// Closes ...
+func (df *DataFrameCandle) Closes() []float64 {
+	s := make([]float64, len(df.Candles))
+	for i, candle := range df.Candles {
+		s[i] = candle.Close
+	}
+	return s
+}
+
+// Highs ...
+func (df *DataFrameCandle) Highs() []float64 {
+	s := make([]float64, len(df.Candles))
+	for i, candle := range df.Candles {
+		s[i] = candle.High
+	}
+	return s
+}
+
+// Low ...
+func (df *DataFrameCandle) Low() []float64 {
+	s := make([]float64, len(df.Candles))
+	for i, candle := range df.Candles {
+		s[i] = candle.Low
+	}
+	return s
+}
+
+// Volume ...
+func (df *DataFrameCandle) Volume() []float64 {
+	s := make([]float64, len(df.Candles))
+	for i, candle := range df.Candles {
+		s[i] = candle.Volume
+	}
+	return s
+}
