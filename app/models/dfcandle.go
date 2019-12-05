d0b73022d830d65dbd7fa46148ab866fa28bc6a0
yutomasamura <yuto.masumura@favy.co.jp>
2019-12-05 09:42:49 +0900

    select_indicators_match_the_market_price

--- app/models/dfcandle.go
+++ app/models/dfcandle.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"cryptocurrency_trading/config"
 	"cryptocurrency_trading/tradingalgo"
+	"sort"
 	"time"
 
 	"github.com/markcheno/go-talib"
@@ -465,4 +467,75 @@ func (df *DataFrameCandle) OptimizeRsi() (performance float64, bestPeriod int, b
 		}
 	}
 	return performance, bestPeriod, bestBuyThread, bestSellThread
+}
+
+// TradeParams ...
+type TradeParams struct {
+	EmaEnable        bool
+	EmaPeriod1       int
+	EmaPeriod2       int
+	BbEnable         bool
+	BbN              int
+	BbK              float64
+	IchimokuEnable   bool
+	MacdEnable       bool
+	MacdFastPeriod   int
+	MacdSlowPeriod   int
+	MacdSignalPeriod int
+	RsiEnable        bool
+	RsiPeriod        int
+	RsiBuyThread     float64
+	RsiSellThread    float64
+}
+
+// Ranking ...
+type Ranking struct {
+	Enable      bool
+	Performance float64
+}
+
+// OptimizeParams ...
+func (df *DataFrameCandle) OptimizeParams() *TradeParams {
+	emaPerformance, emaPeriod1, emaPeriod2 := df.OptimizeEma()
+	bbPerformance, bbN, bbK := df.OptimizeBb()
+	macdPerformance, macdFastPeriod, macdSlowPeriod, macdSignalPeriod := df.OptimizeMacd()
+	ichimokuPerforamcne := df.OptimizeIchimoku()
+	rsiPerformance, rsiPeriod, rsiBuyThread, rsiSellThread := df.OptimizeRsi()
+
+	emaRanking := &Ranking{false, emaPerformance}
+	bbRanking := &Ranking{false, bbPerformance}
+	macdRanking := &Ranking{false, macdPerformance}
+	ichimokuRanking := &Ranking{false, ichimokuPerforamcne}
+	rsiRanking := &Ranking{false, rsiPerformance}
+
+	rankings := []*Ranking{emaRanking, bbRanking, macdRanking, ichimokuRanking, rsiRanking}
+	sort.Slice(rankings, func(i, j int) bool { return rankings[i].Performance > rankings[j].Performance })
+
+	for i, ranking := range rankings {
+		if i >= config.Config.NumRanking {
+			break
+		}
+		if ranking.Performance > 0 {
+			ranking.Enable = true
+		}
+	}
+
+	tradeParams := &TradeParams{
+		EmaEnable:        emaRanking.Enable,
+		EmaPeriod1:       emaPeriod1,
+		EmaPeriod2:       emaPeriod2,
+		BbEnable:         bbRanking.Enable,
+		BbN:              bbN,
+		BbK:              bbK,
+		IchimokuEnable:   ichimokuRanking.Enable,
+		MacdEnable:       macdRanking.Enable,
+		MacdFastPeriod:   macdFastPeriod,
+		MacdSlowPeriod:   macdSlowPeriod,
+		MacdSignalPeriod: macdSignalPeriod,
+		RsiEnable:        rsiRanking.Enable,
+		RsiPeriod:        rsiPeriod,
+		RsiBuyThread:     rsiBuyThread,
+		RsiSellThread:    rsiSellThread,
+	}
+	return tradeParams
 }
--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"cryptocurrency_trading/app/controllers"
+	"cryptocurrency_trading/app/models"
 	"cryptocurrency_trading/config"
 	"cryptocurrency_trading/utils"
+	"fmt"
 	"log"
+	"time"
 )
 
 func main() {
+	df, _ := models.GetAllCandle(config.Config.ProductCode, time.Minute, 365)
+	fmt.Printf("%+v\n", df.OptimizeParams())
 	utils.LoggingSettings(config.Config.LogFile)
 	controllers.StreamIngestionData()
-	controllers.StartWebServer()
 	log.Println(controllers.StartWebServer())
 }
